19c241b29a36395f3cf77e0e0b2e0fcb7b80de59
hossein karbasi <hoseinemto@gmail.com>
2022-01-14 21:59:08 +0330

    Initial Commit

---
+++ app/src/main/java/com/example/twosession/resycle.kt
@@ -0,0 +1,11 @@
+package com.example.twosession
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class resycle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resycle)
+    }
+}
